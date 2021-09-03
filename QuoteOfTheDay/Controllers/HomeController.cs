e3b33cbcc6538732eafa4c5a2f8282dfd99efd3e
ogubuike Alex <ogubuikealex@gmail.com>
2021-09-03 06:43:26 +0100

    Updated Index to show newly added quotes

--- QuoteOfTheDay/Controllers/HomeController.cs
+++ QuoteOfTheDay/Controllers/HomeController.cs
@@ -6,13 +6,7 @@ using QuoteOfTheDay.Models;
 namespace QuoteOfTheDay.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    {       
 
         public IActionResult Index()
         {
